e11af0bfe05a71bc459d92e2cab57ff9ab8269d9
kdrbipul <kdrbipul@gmail.com>
2023-06-19 13:46:35 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ while(count<5){
     console.log("count : "+count);
     count ++;
 }
+
+// difference between for loop and a while loop
+
+for (let i = 1; i <=5; i++){
+    console.log("For loop: "+i);
+}
+
+let i = 1;
+while(i<=5){
+    console.log("While loop: "+i);
+    i++;
+}
